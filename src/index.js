9f98afd09cb110126b71cd83fa0486c5b6333d5e
Clif <42706685+clifhodges13@users.noreply.github.com>
2019-12-29 17:41:52 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,11 +13,15 @@ axios
   .then(response => {
     const data = response.data
     console.log(data)
+    // reference the main video in the DOM
     const mainVideo = document.querySelector('iframe.main-video')
+    // set the default source to the first video in the array from the api
     mainVideo.src = `https://www.youtube.com/embed/${data.items['0'].snippet.resourceId.videoId}`
+    // pass the data along to the next .then()
     return data
   })
   .then(data => {
+    // call the resultsLoop function, passsing the data along as its only parameter
     resultsLoop(data)
   })
   .catch(error => {
@@ -25,21 +29,25 @@ axios
   })
 
 function resultsLoop(data) {
+  // get the container and main video DOM elements
   const cardContainer = document.querySelector('.container')
   const mainVideo = document.querySelector('iframe.main-video')
 
   data.items.forEach(item => {
+    // for each of the data, store the thumb, title, description, and vidId in a variable
     const thumb = item.snippet.thumbnails.default.url
     const title = item.snippet.title
     const description = `${item.snippet.description.substring(0, 250)}...`
     const vidId = item.snippet.resourceId.videoId
 
+    // now append a new card to the card container at each iteration. The youtubeCardCreator handles the shape of the card
     cardContainer.appendChild(youtubeCardCreator(thumb, title, description, vidId, mainVideo))
   })
 }
 
 
 function youtubeCardCreator(thumb, titleText, descText, dataKey, main) {
+// create DOM elements that will be that will be appended onto each new card
   const card = document.createElement('article')
   card.classList.add('video-article')
   card.setAttribute('data-key', dataKey)
@@ -61,15 +69,13 @@ function youtubeCardCreator(thumb, titleText, descText, dataKey, main) {
   desc.textContent = descText
   details.appendChild(desc)
 
-  const pullMain = function (main) {
-    return pullMain(main)
-  }
-
+//   attach a click listener that will call the changeVidSource function, which, well, changes the main video source :)
   card.addEventListener('click', () => {
     changeVidSource(main)
   })
 
   function changeVidSource(main) {
+//   dynamically updates the main video source based on the dataKey, which is the ID of the video of the clicked card
     main.src = `https://www.youtube.com/embed/${dataKey}`
   }
 
